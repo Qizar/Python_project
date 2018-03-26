f2976b74fe74914024fbc51868c07328c3d16b60
Qizar <35865384+Qizar@users.noreply.github.com>
2018-03-26 15:15:49 +0530

    Add files via upload

---
+++ calc_hourly_salary.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        a = float(input("Enter the yearly salary: "))
+        b = a/12
+        c = b/22
+        d = c/8
+        print("Monthly salary: ",b)
+        print("Per day salary: ",c)
+        print("Per hour salary is: ",d,'\n')
+    except:
+        print("You can't enter anything, other than numbers.\n")
+        
